deb25b1acac9ff47a913799eaf75a04d9a312cd2
Alexandra <jenniferalexandra.thompson@mail.utoronto.ca>
2020-05-03 16:14:51 -0400

    All HTML and CSS done for calculator app! Currently, the screen only displays the last button pressed.

--- JS, HTML, CSS/Calculator Project/Calculator.js
+++ JS, HTML, CSS/Calculator Project/Calculator.js
@@ -1,1 +1,14 @@
 
+
+
+const paragraph = document.querySelector('.display-text');
+const all_buttons = document.querySelectorAll('.row-buttons');
+
+
+for (let i = 0; i< (all_buttons.length);i++ ){
+  console.log(1);
+
+  all_buttons[i].addEventListener('click', function(event) {
+      paragraph.innerText = event.target.innerText;
+  });
+}
